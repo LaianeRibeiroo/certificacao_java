2e2a87d05be92999399e7c570e922d622974ce0a
laianeiftm <laiane.ribeiro@sankhya.com.br>
2022-03-15 10:11:35 -0300

    Otimiza todo o código implementado

--- univesp/AreaCasa.java
+++ univesp/AreaCasa.java
@@ -1,48 +1,38 @@
 class AreaCasa { 
 	
+	//Valor do metro quadrado
 	static double valorM2 = 1500;  
-    static final int ALVENARIA = 0; 
+    
+	//Materiais da psicina
+	static final int ALVENARIA = 0; 
 	static final int VINIL = 1;
 	static final int FIBRA = 2;
 	static final int PLASTICO = 3;
 	
+	//Preços dos materiais
     static double [] precos = {1500, 1100, 750, 500};
 	
+	//Area da casa
 	static void areacasa( float lateral, float cquarto){  //inicializando método com passagem de parametros. 
 	
 	    float  areaq, areas, areat; // área do quarto, sala e total;
-    
-	    System.out.println("Programa para cálculo da área da casa");
-			
+        System.out.println("Programa para cálculo da área da casa");
 		areas = lateral * lateral;
 		System.out.println("A área da sala é " + areas);
-      
-		areaq = cquarto* (lateral/2); // areaq = 7 * 5;;
-		
-		// saida no terminal informando as areas
+      	areaq = cquarto* (lateral/2); // areaq = 7 * 5;;
 		System.out.println("A área do quarto é " + areaq);
 		System.out.println("A área do banheiro é " + areaq);
-        
-		areat = areas + 2 * areaq; //cauculo da area total
-		
-    	System.out.println("A área total é " + areat); // saida no terminal informando a area total
+        areat = areas + 2 * areaq; 
+		System.out.println("A área total é " + areat); 
 	}
 	
-	 /* static double valorPiscina(double area, int material){
-	   switch (material){
-		case ALVENARIA: return (area*1500);
-        case VINIL: return (area*1100);			               
-		case FIBRA: return (area*750);			               
-		case PLASTICO: return (area*500);			               
-		default: return (-1);
-	
-		if (material <ALVENARIA || material > PLASTICO || AREA < 0)
-			return (-1);
-		
-		
-		}return (area*precos[material]; 	*/ 
-	
-	   	
+	// valor do metro de acordo com o material 
+	static double valorPiscina(double area, int material){
+	  		if (material <ALVENARIA || material > PLASTICO || area < 0)
+			  return (-1);
+		 return (area * precos[material]); 	
+	}
+	// cauculo da area total   	
 	static double areaPiscina (double raio, int material) { 
 		double valor;
 		double resp;
@@ -54,15 +44,16 @@ class AreaCasa {
 		return resp;
 	}
 	
+	//Cauculo do valor por metro quadrado
 	static double valor (double area){
-		if (area >= 0 ){ // testando se a area é positiva, se sim, o cauculo será executado
+		if (area >= 0 ){ 
 		 return (valorM2*area); 	
 		}
 		return (-1); 
 	}
 		
-		
-    static double media(double [] arranjo){ //metodo de caulculo da média
+	//metodo de caulculo da média	
+    static double media(double [] arranjo){ 
 		double resp=0;
 		for (double valor: arranjo){
 		resp += valor;	
@@ -71,14 +62,7 @@ class AreaCasa {
 	}
 	
 	
-	/* public static void main(String[] args) { 
-	double [] precos2 = new double [4];
-	
-	precos2[ALVENARIA] = 1500;
-	precos2[VINIL] = 1100;
-	precos2[FIBRA] = 750;
-	precos2[PLASTICO] = 500; */
-	
+     //Cauculo da area usando arranjo	
      public static void main(String[] args) { 
       double [] precos = {1500, 1100, 750, 500};
          for (int i = 0; i<4 ; i++){
