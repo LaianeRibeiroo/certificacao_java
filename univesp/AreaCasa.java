97706ed0d467b4de26343a45ed2e8b3ca00eb6da
laianeiftm <laiane.ribeiro@sankhya.com.br>
2022-03-31 14:22:16 -0300

    Implementa separação classe Casa e Piscina - Aula 19

--- univesp/AreaCasa.java
+++ univesp/AreaCasa.java
@@ -1,49 +1,28 @@
+ /* Programa para caucular a area de uma casa ( e seus comodos) de 3 comodos: Uma
+ sala de 10x10m, um banheiro e um quarto de 5x7m cada. */ 
+ 
+ 
  class AreaCasa { 
 	
-	//Valor do metro quadrado
+	//Valor do metro quadrado da casa
 	static double valorM2 = 1500;  
-    
-	//Materiais da psicina
-	static final int ALVENARIA = 0; 
-	static final int VINIL = 1;
-	static final int FIBRA = 2;
-	static final int PLASTICO = 3;
+   
 	
-	//Preços dos materiais
-    static double [] precos = {1500, 1100, 750, 500};
-	
-	//Area da casa
+	//Caucula a Area da casa
 	static void areacasa( float lateral, float cquarto){  
+	float areaq;
+	float areas; 
+	float areat; 
 	
-	    float  areaq, areas, areat; 
-        System.out.println("Programa para cálculo da área da casa");
-		areas = lateral * lateral;
-		System.out.println("A área da sala é " + areas);
-      	areaq = cquarto* (lateral/2); 
-		System.out.println("A área do quarto é " + areaq);
-		System.out.println("A área do banheiro é " + areaq);
-        areat = areas + 2 * areaq; 
-		System.out.println("A área total é " + areat); 
+	//calculo da area
+	static double area (double lateral, double cquarto){
+		 if (lateral>=0 && cquarto >=0{
+			 areat = lateral * lateral; 
+			 areat += cquarto * lateral; 
+		 } return (areat);
 	}
 	
-	// valor do metro de acordo com o material 
-	static double valorPiscina(double area, int material){
-	  		if (material <ALVENARIA || material > PLASTICO || area < 0)
-			  return (-1);
-		 return (area * precos[material]); 	
-	}
-	// cauculo da area total   	
-	static double areaPiscina (double raio, int material) { 
-		double valor;
-		double resp;
-		double area = 100;
-		if (raio >= 0) {
-		 resp = Math.PI * Math.pow(raio,2); 
-		} 
-		else resp = -1;
-		return resp;
-	}
-	
+  
 	//Cauculo do valor por metro quadrado
 	static double valor (double area){
 		if (area >= 0 ){ 
@@ -68,36 +47,32 @@
           }
         }
       }
-       public static double[][]cauculaFinal(double [][] val,double [][] desc){
-	      double [][] saida = new double [val.length] [val[0].length];
-		  for (int i=0; i<saida.length; i++){
-			  for (int j=0; i<saida[0].length; j++){
-			saida [i][j] = val [i][j] * (1-desc[i][j]);
-	          }
-	      }
-		  return(saida);
-	    }
-		  
-	   
-	   
+      
+      double[][] calculaFinal(double[][] val, double[][] desc){
+        double[][] saida = new double[val.length][val[0].length];
 
-      public static void main(String[] args) { 
-       double [][] valores = new double[4][4];
-       double [][] descontos = { 
-		   {0,0,0.2,0.2},
-		   {0.05,0.05,0.1,0.15},
-		   {0.02,0.04,0.08,0.16},
-	       {0,0,0,0.05}};
-	   double [][] pFinal;
-	   carregaval(valores);
-	   pFinal = cauculaFinal(valores,descontos);
-	      for (double [] linha: valores){
-			for (double valor: linha)
-				System.out.print(valor + " | ");
-			 System.out.println();
+        for(int i = 0; i < saida.length; i++){
+            for(int j = 0; j < saida[0].length ; j++){
+                saida[i][j] = val[i][j] * (1 - desc[i][j]);
+            }
+        }
+
+        return (saida);
+     }
+	
+    
+		
+		static void imprimiMatriz(double [][] matriz){
+		  for(int i=0; i<matriz.length; i++){
+			for (int j=0; i<matriz[0].length; j++){
+			  System.out.print(matriz [i][j] + " \t | ");   
+		    }
+			System.out.println();
 		  }
-	  }    
-
+		}
+	
+	
+	
 }
 
 	 
--- univesp/AreaPiscina.java
+++ univesp/AreaPiscina.java
@@ -2,15 +2,33 @@
 casa. Produzido com base no curso Programação de Computadores da UNIVESP */ 
 
 class AreaPiscina {
-	public static void main ( String [] args){ // declarando método main;
+	    
+	//Materiais da psicina
+	static final int ALVENARIA = 0; 
+	static final int VINIL = 1;
+	static final int FIBRA = 2;
+	static final int PLASTICO = 3;
+	
+	//Preços dos materiais
+    static double [] precos = {1500, 1100, 750, 500};
+	
+	//nomes dos materiais 
+	static char [][] nomes = 
+	 {{'A','l','v','e','n','a','r','i','a'}
+	 {'V','i','n','i','l'}
+	 {'F','i','b','r','a'}
+	 {'P','l','á','s','t','i','c','o'}};
+	
+	// cauculo da area total   	
+	static double areaPiscina (double raio, int material) { 
+	 return(raio >= 0) ?
+	  Math.PI * Math.pow(raio,2):-1; 
+		} 
+	// valor do metro de acordo com o material 
+	static double valorPiscina(double area, int material){
+	 if (material <ALVENARIA || material > PLASTICO || area < 0)
+	  return (-1);
+		 return (area * precos[material]); 	
+	}
 		
-		//declarando as variaveis que serão utilizadas no cauculo; 
-		double raio =2;
-		double areap;
-		// operação de cauculo da area utilizando o pi e potencia da biblioteca math do java; 
-		areap = Math.PI * Math.pow(raio,2);
-		
-		//saida no terminal informando a area
-		System.out.println ("area: " + areap);
-	}   
 }
